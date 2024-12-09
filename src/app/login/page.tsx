e2e9ba2100d9dbda80a776325485ced11412dfe2
mjhn010 <mjhn010@naver.com>
2024-12-09 16:45:27 +0900

    fix : gitLoginSession props 변경

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 import MainBackground from "../../../public/images/bg-circle.svg";
 import BackgroundCircles from "@/components/landing/head-article/BackgroundCircles";
 import { Metadata } from "next";
+import { getSession } from "@/lib/getSession";
 
 export const metadata: Metadata = {
   title: "Login",
 };
-function Page() {
+async function Page() {
+  const session = getSession();
   return (
     <article className="relative flex h-[1172px] min-w-[1920px] items-center justify-center">
       {/* 배경 이미지 */}
@@ -16,7 +18,7 @@ function Page() {
         src={MainBackground}
         alt="Main Background"
         fill
-        className="animate-fadeIn -z-20 object-cover"
+        className="-z-20 animate-fadeIn object-cover"
       />
       <div className="flex h-[509px] flex-col items-center justify-center gap-[65px]">
         <div className="flex items-center justify-center gap-60">
@@ -28,7 +30,7 @@ function Page() {
               Login
             </span>
           </h1>
-          <GithubLogin />
+          <GithubLogin session={session} />
         </div>
       </div>
     </article>
