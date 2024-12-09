2ec4f1e5b1ebab52db071ce6a4f1188887c09630
mjhn010 <mjhn010@naver.com>
2024-12-09 20:19:31 +0900

    fix : 챗봇 코드 수정 및 git reset --hard HEAD 변경

--- src/app/api/chatBot/route.ts
+++ src/app/api/chatBot/route.ts
@@ -1,39 +1,18 @@
 import { NextResponse } from "next/server";
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 export async function POST(req: Request) {
   const body = await req.json();
 
   console.log(body);
   try {
-    const auth_url = "http://3.34.255.163:8000/auth/token";
-    const formData = new URLSearchParams();
-    formData.append("username", `${process.env.NEXT_PUBLIC_USERNAME}`);
-    formData.append("password", `${process.env.NEXT_PUBLIC_PASSWORD}`);
-    const tokenResponse = await fetch(auth_url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: formData,
-    });
-    const token = await tokenResponse.json();
-    console.log("Token:", token);
-    const resultRequest = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token.access_token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_message: `${body}`,
-        temperature: 0.1,
-        top_p: 0.1,
-      }),
-    };
-    const result = await fetch(
-      `http://3.34.255.163:8000/generate`,
-      resultRequest,
-    ).then((res) => res.text());
-    console.log(result);
+    const genAI = new GoogleGenerativeAI(
+      process.env.NEXT_PUBLIC_GEMINI_API_KEY,
+    );
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const prompt = body;
+    const result = await model.generateContent(prompt);
+    const resultAI = result.response.text();
+    return NextResponse.json(resultAI);
     return NextResponse.json(result);
   } catch (error) {
     console.log(error);
